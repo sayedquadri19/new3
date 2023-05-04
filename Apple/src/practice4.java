664c4e796e20f29cb7d891c18d7875094d70c3ca
user <user@DESKTOP-FG4DA2N>
2023-05-04 10:47:27 +0530

    second commit

--- Apple/src/practice4.java
+++ Apple/src/practice4.java
@@ -3,6 +3,11 @@ public class practice4 {
 
 	public static void main(String[] args) {
 		System.out.println("one");
+		System.out.println("two");
+		System.out.println("one");
+		
+		
+		
 		// TODO Auto-generated method stub
 
 	}
